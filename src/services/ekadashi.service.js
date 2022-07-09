0d8ce746df00a822c615dd99abcc38b5b8d4ad31
Miles Felton Gerber <72751046+mwfelton@users.noreply.github.com>
2022-07-09 19:15:47 +0100

    experiment with state management

--- src/services/ekadashi.service.js
+++ src/services/ekadashi.service.js
@@ -1,24 +1,18 @@
-// import { mocks } from "./mock";
-// import camelize from "camelize";
+import { ekadashiByYears } from './local-data'
 
-// export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
-//   return new Promise((resolve, reject) => {
-//     const mock = mocks[location];
-//     if (!mock) {
-//       reject("not found");
-//     }
-//     resolve(mock);
-//   });
-// };
-
-// export const restaurantsTransform = ({ results = [] }) => {
-//   const mappedResults = results.map((restaurant) => {
-//     return {
-//       ...restaurant,
-//       isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
-//       isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
-//     };
-//   });
-
-//   return camelize(mappedResults);
-// };
+export const ekadashiDateRequest = (year = "2022") => {
+  return new Promise((resolve, reject) => {
+    const year = ekadashiByYears[year];
+    if (!year) {
+      reject("not found");
+    }
+    resolve(year);
+  });
+};
+ekadashiDateRequest()
+  .then((result) => {
+    console.log(result);
+  })
+  .catch((err) => {
+    console.log("error");
+  });
---
+++ src/services/local-data/index.js
@@ -0,0 +1,7 @@
+import ekadashi2022 from "./ekadashi-2022.json"
+import ekadashi2023 from "./ekadashi-2022 copy.json"
+
+export const ekadashiByYears = {
+    "2022": ekadashi2022,
+    "2023": ekadashi2023,
+}
