f938cc64bd2b68b0024dad83573b28781b6f2f7d
Miles Felton Gerber <mwfelton03@gmail.com>
2022-07-10 18:10:47 +0100

    progressing with context and firebase

--- App.js
+++ App.js
@@ -5,13 +5,16 @@ import { NavigationContainer } from '@react-navigation/native';
 
 import { theme } from "./src/infrastructure/theme/"
 import StackNavigator from "./src/utils/navigation/StackNavigator";
+import { AuthProvider } from "./src/utils/hooks/useAuth";
 
 export default function App() {
   return (
     <>
     <ThemeProvider theme={theme}>
       <NavigationContainer>
-        <StackNavigator />
+        <AuthProvider>
+          <StackNavigator />
+        </AuthProvider>
       </NavigationContainer>
     </ThemeProvider>
     <ExpoStatusBar style="auto" />
--- src/utils/navigation/StackNavigator.js
+++ src/utils/navigation/StackNavigator.js
@@ -5,15 +5,28 @@ import LoginScreen from "../../screens/login.screen";
 import UserLogin from '../../components/auth/UserLogin'
 import UserRegister from '../../components/auth/UserRegister';
 import { EkadashiInfoScreen } from '../../screens/ekadashi-info/ekadashi-home.screen';
+import useAuth from '../hooks/useAuth';
 
 const Stack = createNativeStackNavigator();
 
 const StackNavigator = () => {
+    const { user } = useAuth()
+
   return (
     <Stack.Navigator>
-          <Stack.Screen options={{ headerShown: false }} name="EkadashiInfo" component={EkadashiInfoScreen} />
-          <Stack.Screen name="UserLogin" component={UserLogin} options={{ headerShown: false }} />
-          <Stack.Screen name="Login-screen" component={LoginScreen} options={{ headerShown: false }} />
+        {user ? (
+            <>
+                <Stack.Screen options={{ headerShown: false }} name="EkadashiInfo" component={EkadashiInfoScreen} />
+                <Stack.Screen name="Login-screen" component={LoginScreen} options={{ headerShown: false }} />
+            </>
+        ) : (
+            <>
+                <Stack.Screen options={{ headerShown: false }} name="EkadashiInfo" component={EkadashiInfoScreen} />
+                <Stack.Screen name="UserLogin" component={UserLogin} options={{ headerShown: false }} />
+                <Stack.Screen name="Login-screen" component={LoginScreen} options={{ headerShown: false }} />
+            </>
+        )
+        }
     </Stack.Navigator>
   )
 }
